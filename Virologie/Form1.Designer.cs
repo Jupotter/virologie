a70136294c69e2cc3d6ef47ade35120a95f361d9
Quentin Leprael <leprae_q@epita.fr>
2014-07-16 16:53:29 +0200

    Début d'interface

--- Virologie/Form1.Designer.cs
+++ Virologie/Form1.Designer.cs
@@ -28,12 +28,307 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.HomeLabel = new System.Windows.Forms.Label();
+            this.ScanLabel = new System.Windows.Forms.Label();
+            this.SecurityLabel = new System.Windows.Forms.Label();
+            this.WindowsTextBox = new System.Windows.Forms.RichTextBox();
+            this.WindowsLabel = new System.Windows.Forms.Label();
+            this.StayProtectedLabel = new System.Windows.Forms.Label();
+            this.UpdateLabel = new System.Windows.Forms.Label();
+            this.SettingsLabel = new System.Windows.Forms.Label();
+            this.WarningTextBox = new System.Windows.Forms.RichTextBox();
+            this.SystemLabel = new System.Windows.Forms.Label();
+            this.LastScanLabel = new System.Windows.Forms.Label();
+            this.DangerPictureBox = new System.Windows.Forms.PictureBox();
+            this.SettingsCheckBox = new System.Windows.Forms.CheckBox();
+            this.UpdateCheckBox = new System.Windows.Forms.CheckBox();
+            this.ScanCheckBox = new System.Windows.Forms.CheckBox();
+            this.SecurityCheckBox = new System.Windows.Forms.CheckBox();
+            this.HomeCheckBox = new System.Windows.Forms.CheckBox();
+            this.LogoPictureBox = new System.Windows.Forms.PictureBox();
+            this.ScanRedirectLabel = new System.Windows.Forms.LinkLabel();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DangerPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // HomeLabel
+            // 
+            this.HomeLabel.AutoSize = true;
+            this.HomeLabel.Location = new System.Drawing.Point(43, 153);
+            this.HomeLabel.Name = "HomeLabel";
+            this.HomeLabel.Size = new System.Drawing.Size(35, 13);
+            this.HomeLabel.TabIndex = 1;
+            this.HomeLabel.Text = "Home";
+            // 
+            // ScanLabel
+            // 
+            this.ScanLabel.AutoSize = true;
+            this.ScanLabel.Location = new System.Drawing.Point(43, 248);
+            this.ScanLabel.Name = "ScanLabel";
+            this.ScanLabel.Size = new System.Drawing.Size(32, 13);
+            this.ScanLabel.TabIndex = 3;
+            this.ScanLabel.Text = "Scan";
+            // 
+            // SecurityLabel
+            // 
+            this.SecurityLabel.AutoSize = true;
+            this.SecurityLabel.Location = new System.Drawing.Point(36, 343);
+            this.SecurityLabel.Name = "SecurityLabel";
+            this.SecurityLabel.Size = new System.Drawing.Size(45, 13);
+            this.SecurityLabel.TabIndex = 5;
+            this.SecurityLabel.Text = "Security";
+            // 
+            // WindowsTextBox
+            // 
+            this.WindowsTextBox.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.WindowsTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.WindowsTextBox.Location = new System.Drawing.Point(0, 0);
+            this.WindowsTextBox.Name = "WindowsTextBox";
+            this.WindowsTextBox.Size = new System.Drawing.Size(800, 70);
+            this.WindowsTextBox.TabIndex = 6;
+            this.WindowsTextBox.Text = "";
+            // 
+            // WindowsLabel
+            // 
+            this.WindowsLabel.AutoSize = true;
+            this.WindowsLabel.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.WindowsLabel.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WindowsLabel.ForeColor = System.Drawing.SystemColors.Info;
+            this.WindowsLabel.Location = new System.Drawing.Point(68, 9);
+            this.WindowsLabel.Name = "WindowsLabel";
+            this.WindowsLabel.Size = new System.Drawing.Size(227, 18);
+            this.WindowsLabel.TabIndex = 7;
+            this.WindowsLabel.Text = "Windows AntiVir Defender 2014";
+            // 
+            // StayProtectedLabel
+            // 
+            this.StayProtectedLabel.AutoSize = true;
+            this.StayProtectedLabel.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.StayProtectedLabel.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StayProtectedLabel.ForeColor = System.Drawing.SystemColors.Info;
+            this.StayProtectedLabel.Location = new System.Drawing.Point(68, 27);
+            this.StayProtectedLabel.Name = "StayProtectedLabel";
+            this.StayProtectedLabel.Size = new System.Drawing.Size(253, 17);
+            this.StayProtectedLabel.TabIndex = 8;
+            this.StayProtectedLabel.Text = "Stay protected anywhere, at anytime !";
+            // 
+            // UpdateLabel
+            // 
+            this.UpdateLabel.AutoSize = true;
+            this.UpdateLabel.Location = new System.Drawing.Point(38, 438);
+            this.UpdateLabel.Name = "UpdateLabel";
+            this.UpdateLabel.Size = new System.Drawing.Size(42, 13);
+            this.UpdateLabel.TabIndex = 11;
+            this.UpdateLabel.Text = "Update";
+            // 
+            // SettingsLabel
+            // 
+            this.SettingsLabel.AutoSize = true;
+            this.SettingsLabel.Location = new System.Drawing.Point(36, 533);
+            this.SettingsLabel.Name = "SettingsLabel";
+            this.SettingsLabel.Size = new System.Drawing.Size(45, 13);
+            this.SettingsLabel.TabIndex = 13;
+            this.SettingsLabel.Text = "Settings";
+            // 
+            // WarningTextBox
+            // 
+            this.WarningTextBox.BackColor = System.Drawing.Color.DarkOrange;
+            this.WarningTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.WarningTextBox.Location = new System.Drawing.Point(120, 86);
+            this.WarningTextBox.Name = "WarningTextBox";
+            this.WarningTextBox.Size = new System.Drawing.Size(650, 93);
+            this.WarningTextBox.TabIndex = 19;
+            this.WarningTextBox.Text = "";
+            // 
+            // SystemLabel
+            // 
+            this.SystemLabel.AutoSize = true;
+            this.SystemLabel.BackColor = System.Drawing.Color.DarkOrange;
+            this.SystemLabel.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SystemLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.SystemLabel.Location = new System.Drawing.Point(220, 106);
+            this.SystemLabel.Name = "SystemLabel";
+            this.SystemLabel.Size = new System.Drawing.Size(184, 22);
+            this.SystemLabel.TabIndex = 20;
+            this.SystemLabel.Text = "System in Danger !";
+            // 
+            // LastScanLabel
+            // 
+            this.LastScanLabel.AutoSize = true;
+            this.LastScanLabel.BackColor = System.Drawing.Color.DarkOrange;
+            this.LastScanLabel.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LastScanLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.LastScanLabel.Location = new System.Drawing.Point(220, 145);
+            this.LastScanLabel.Name = "LastScanLabel";
+            this.LastScanLabel.Size = new System.Drawing.Size(190, 16);
+            this.LastScanLabel.TabIndex = 21;
+            this.LastScanLabel.Text = "Last Scan : Never Performed";
+            // 
+            // DangerPictureBox
+            // 
+            this.DangerPictureBox.BackgroundImage = global::Virologie.Properties.Resources.Danger;
+            this.DangerPictureBox.Location = new System.Drawing.Point(143, 104);
+            this.DangerPictureBox.Name = "DangerPictureBox";
+            this.DangerPictureBox.Size = new System.Drawing.Size(60, 60);
+            this.DangerPictureBox.TabIndex = 22;
+            this.DangerPictureBox.TabStop = false;
+            // 
+            // SettingsCheckBox
+            // 
+            this.SettingsCheckBox.Appearance = System.Windows.Forms.Appearance.Button;
+            this.SettingsCheckBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("SettingsCheckBox.BackgroundImage")));
+            this.SettingsCheckBox.Location = new System.Drawing.Point(30, 470);
+            this.SettingsCheckBox.Name = "SettingsCheckBox";
+            this.SettingsCheckBox.Size = new System.Drawing.Size(60, 60);
+            this.SettingsCheckBox.TabIndex = 18;
+            this.SettingsCheckBox.UseVisualStyleBackColor = true;
+            this.SettingsCheckBox.Click += new System.EventHandler(this.SettingsCheckBox_Click);
+            // 
+            // UpdateCheckBox
+            // 
+            this.UpdateCheckBox.Appearance = System.Windows.Forms.Appearance.Button;
+            this.UpdateCheckBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("UpdateCheckBox.BackgroundImage")));
+            this.UpdateCheckBox.Location = new System.Drawing.Point(30, 375);
+            this.UpdateCheckBox.Name = "UpdateCheckBox";
+            this.UpdateCheckBox.Size = new System.Drawing.Size(60, 60);
+            this.UpdateCheckBox.TabIndex = 17;
+            this.UpdateCheckBox.UseVisualStyleBackColor = true;
+            this.UpdateCheckBox.Click += new System.EventHandler(this.UpdateCheckBox_Click);
+            // 
+            // ScanCheckBox
+            // 
+            this.ScanCheckBox.Appearance = System.Windows.Forms.Appearance.Button;
+            this.ScanCheckBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ScanCheckBox.BackgroundImage")));
+            this.ScanCheckBox.Location = new System.Drawing.Point(30, 185);
+            this.ScanCheckBox.Name = "ScanCheckBox";
+            this.ScanCheckBox.Size = new System.Drawing.Size(60, 60);
+            this.ScanCheckBox.TabIndex = 16;
+            this.ScanCheckBox.UseVisualStyleBackColor = true;
+            this.ScanCheckBox.Click += new System.EventHandler(this.ScanCheckBox_Click);
+            // 
+            // SecurityCheckBox
+            // 
+            this.SecurityCheckBox.Appearance = System.Windows.Forms.Appearance.Button;
+            this.SecurityCheckBox.BackgroundImage = global::Virologie.Properties.Resources.Shield;
+            this.SecurityCheckBox.Location = new System.Drawing.Point(30, 280);
+            this.SecurityCheckBox.Name = "SecurityCheckBox";
+            this.SecurityCheckBox.Size = new System.Drawing.Size(60, 60);
+            this.SecurityCheckBox.TabIndex = 15;
+            this.SecurityCheckBox.UseVisualStyleBackColor = true;
+            this.SecurityCheckBox.Click += new System.EventHandler(this.SecurityCheckBox_Click);
+            // 
+            // HomeCheckBox
+            // 
+            this.HomeCheckBox.Appearance = System.Windows.Forms.Appearance.Button;
+            this.HomeCheckBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("HomeCheckBox.BackgroundImage")));
+            this.HomeCheckBox.Location = new System.Drawing.Point(30, 90);
+            this.HomeCheckBox.Name = "HomeCheckBox";
+            this.HomeCheckBox.Size = new System.Drawing.Size(60, 60);
+            this.HomeCheckBox.TabIndex = 14;
+            this.HomeCheckBox.UseVisualStyleBackColor = true;
+            this.HomeCheckBox.Click += new System.EventHandler(this.HomeCheckBox_Click);
+            // 
+            // LogoPictureBox
+            // 
+            this.LogoPictureBox.Image = global::Virologie.Properties.Resources.Logo;
+            this.LogoPictureBox.Location = new System.Drawing.Point(12, 9);
+            this.LogoPictureBox.Name = "LogoPictureBox";
+            this.LogoPictureBox.Size = new System.Drawing.Size(50, 50);
+            this.LogoPictureBox.TabIndex = 9;
+            this.LogoPictureBox.TabStop = false;
+            // 
+            // ScanRedirectLabel
+            // 
+            this.ScanRedirectLabel.AutoSize = true;
+            this.ScanRedirectLabel.BackColor = System.Drawing.Color.DarkOrange;
+            this.ScanRedirectLabel.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScanRedirectLabel.Location = new System.Drawing.Point(460, 123);
+            this.ScanRedirectLabel.Name = "ScanRedirectLabel";
+            this.ScanRedirectLabel.Size = new System.Drawing.Size(259, 16);
+            this.ScanRedirectLabel.TabIndex = 23;
+            this.ScanRedirectLabel.TabStop = true;
+            this.ScanRedirectLabel.Text = "Click here to start scanning your system";
+            this.ScanRedirectLabel.Click += new System.EventHandler(this.ScanCheckBox_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Location = new System.Drawing.Point(120, 209);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(650, 50);
+            this.richTextBox2.TabIndex = 24;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(146, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(798, 566);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.ScanRedirectLabel);
+            this.Controls.Add(this.DangerPictureBox);
+            this.Controls.Add(this.LastScanLabel);
+            this.Controls.Add(this.SystemLabel);
+            this.Controls.Add(this.WarningTextBox);
+            this.Controls.Add(this.SettingsCheckBox);
+            this.Controls.Add(this.UpdateCheckBox);
+            this.Controls.Add(this.ScanCheckBox);
+            this.Controls.Add(this.SecurityCheckBox);
+            this.Controls.Add(this.HomeCheckBox);
+            this.Controls.Add(this.SettingsLabel);
+            this.Controls.Add(this.UpdateLabel);
+            this.Controls.Add(this.LogoPictureBox);
+            this.Controls.Add(this.StayProtectedLabel);
+            this.Controls.Add(this.WindowsLabel);
+            this.Controls.Add(this.WindowsTextBox);
+            this.Controls.Add(this.SecurityLabel);
+            this.Controls.Add(this.ScanLabel);
+            this.Controls.Add(this.HomeLabel);
+            this.Name = "Form1";
+            this.Text = "Windows AntiVir Defender 2014";
+            ((System.ComponentModel.ISupportInitialize)(this.DangerPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label HomeLabel;
+        private System.Windows.Forms.Label ScanLabel;
+        private System.Windows.Forms.Label SecurityLabel;
+        private System.Windows.Forms.RichTextBox WindowsTextBox;
+        private System.Windows.Forms.Label WindowsLabel;
+        private System.Windows.Forms.Label StayProtectedLabel;
+        private System.Windows.Forms.PictureBox LogoPictureBox;
+        private System.Windows.Forms.Label UpdateLabel;
+        private System.Windows.Forms.Label SettingsLabel;
+        private System.Windows.Forms.CheckBox HomeCheckBox;
+        private System.Windows.Forms.CheckBox SecurityCheckBox;
+        private System.Windows.Forms.CheckBox ScanCheckBox;
+        private System.Windows.Forms.CheckBox UpdateCheckBox;
+        private System.Windows.Forms.CheckBox SettingsCheckBox;
+        private System.Windows.Forms.RichTextBox WarningTextBox;
+        private System.Windows.Forms.Label SystemLabel;
+        private System.Windows.Forms.Label LastScanLabel;
+        private System.Windows.Forms.PictureBox DangerPictureBox;
+        private System.Windows.Forms.LinkLabel ScanRedirectLabel;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Virologie/Form1.cs
+++ Virologie/Form1.cs
@@ -11,9 +11,119 @@ namespace Virologie
 {
     public partial class Form1 : Form
     {
+        private List<Component> HomeCollection;
+        private List<Component> ScanCollection;
+        private List<Component> SecurityCollection;
+        private List<Component> UpdateCollection;
+        private List<Component> SettingsCollection;
+
         public Form1()
         {
             InitializeComponent();
+            HomeCheckBox.CheckState = CheckState.Checked;
+
+            HomeCollection = new List<Component>();
+            ScanCollection = new List<Component>();
+            SecurityCollection = new List<Component>();
+            UpdateCollection = new List<Component>();
+            SettingsCollection = new List<Component>();
+
+            HomeCollection.Add(WarningTextBox);
+            HomeCollection.Add(DangerPictureBox);
+            HomeCollection.Add(SystemLabel);
+            HomeCollection.Add(LastScanLabel);
+            HomeCollection.Add(ScanRedirectLabel);
+
+            DisableControlCollection(ScanCollection);
+            DisableControlCollection(SecurityCollection);
+            DisableControlCollection(UpdateCollection);
+            DisableControlCollection(SettingsCollection);
+            EnableControlCollection(HomeCollection);
+        }
+
+        private void EnableControlCollection(List<Component> list)
+        {
+            foreach (Control c in list) c.Visible = true;
+        }
+
+        private void DisableControlCollection(List<Component> list)
+        {
+            foreach (Control c in list) c.Visible = false;
+        }
+
+        private void HomeCheckBox_Click(object sender, EventArgs e)
+        {
+            HomeCheckBox.CheckState = CheckState.Checked;
+            ScanCheckBox.CheckState = CheckState.Unchecked;
+            SecurityCheckBox.CheckState = CheckState.Unchecked;
+            UpdateCheckBox.CheckState = CheckState.Unchecked;
+            SettingsCheckBox.CheckState = CheckState.Unchecked;
+
+            EnableControlCollection(HomeCollection);
+            DisableControlCollection(ScanCollection);
+            DisableControlCollection(SecurityCollection);
+            DisableControlCollection(UpdateCollection);
+            DisableControlCollection(SettingsCollection);
+        }
+
+        private void ScanCheckBox_Click(object sender, EventArgs e)
+        {
+            HomeCheckBox.CheckState = CheckState.Unchecked;
+            ScanCheckBox.CheckState = CheckState.Checked;
+            SecurityCheckBox.CheckState = CheckState.Unchecked;
+            UpdateCheckBox.CheckState = CheckState.Unchecked;
+            SettingsCheckBox.CheckState = CheckState.Unchecked;
+
+            DisableControlCollection(HomeCollection);
+            EnableControlCollection(ScanCollection);
+            DisableControlCollection(SecurityCollection);
+            DisableControlCollection(UpdateCollection);
+            DisableControlCollection(SettingsCollection);
+        }
+
+        private void SecurityCheckBox_Click(object sender, EventArgs e)
+        {
+            HomeCheckBox.CheckState = CheckState.Unchecked;
+            ScanCheckBox.CheckState = CheckState.Unchecked;
+            SecurityCheckBox.CheckState = CheckState.Checked;
+            UpdateCheckBox.CheckState = CheckState.Unchecked;
+            SettingsCheckBox.CheckState = CheckState.Unchecked;
+
+            DisableControlCollection(HomeCollection);
+            DisableControlCollection(ScanCollection);
+            EnableControlCollection(SecurityCollection);
+            DisableControlCollection(UpdateCollection);
+            DisableControlCollection(SettingsCollection);
+        }
+
+        private void UpdateCheckBox_Click(object sender, EventArgs e)
+        {
+            HomeCheckBox.CheckState = CheckState.Unchecked;
+            ScanCheckBox.CheckState = CheckState.Unchecked;
+            SecurityCheckBox.CheckState = CheckState.Unchecked;
+            UpdateCheckBox.CheckState = CheckState.Checked;
+            SettingsCheckBox.CheckState = CheckState.Unchecked;
+
+            DisableControlCollection(HomeCollection);
+            DisableControlCollection(ScanCollection);
+            DisableControlCollection(SecurityCollection);
+            EnableControlCollection(UpdateCollection);
+            DisableControlCollection(SettingsCollection);
+        }
+
+        private void SettingsCheckBox_Click(object sender, EventArgs e)
+        {
+            HomeCheckBox.CheckState = CheckState.Unchecked;
+            ScanCheckBox.CheckState = CheckState.Unchecked;
+            SecurityCheckBox.CheckState = CheckState.Unchecked;
+            UpdateCheckBox.CheckState = CheckState.Unchecked;
+            SettingsCheckBox.CheckState = CheckState.Checked;
+
+            DisableControlCollection(HomeCollection);
+            DisableControlCollection(ScanCollection);
+            DisableControlCollection(SecurityCollection);
+            DisableControlCollection(UpdateCollection);
+            EnableControlCollection(SettingsCollection);
         }
     }
 }
